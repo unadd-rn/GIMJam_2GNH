2018a3d9248975c297e3f83ac3e8092dac5c4656
Chil41 <cettaresmi3@gmail.com>
2026-01-24 19:59:26 +0700

    EnemyShoot

---
+++ GIMJam/Assets/Script/EnemyRobot/EnemyBulletScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float timer;
+    public float force;
+    public bool ToRight;
+    public float DisappearTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        if(ToRight){
+        rb.velocity = new Vector2(1,0)*force;
+        }
+        else
+        {
+            rb.velocity = new Vector2(-1,0)*force;
+        }
+    }
+
+    void Update() 
+    {
+        timer += Time.deltaTime;
+        if(timer>DisappearTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //other.gameObject.GetComponent<sriptyg ada health>().health -= 1
+            Destroy(gameObject);
+        }       
+    }
+}
