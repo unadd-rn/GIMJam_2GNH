0a7a70e92f8f3ec1e1cd76405f38d59b82d36918
unadd-rn <auliaunad@gmail.com>
2026-01-28 14:29:27 +0700

    fix/dialoguetriggeronce

--- GIMJam/Assets/Script/Dialogue/DialogueTrigger.cs
+++ GIMJam/Assets/Script/Dialogue/DialogueTrigger.cs
@@ -6,16 +6,21 @@ public class DialogueTrigger : MonoBehaviour
 {
     [Header("Ink JSON")]
     [SerializeField] private TextAsset inkJSON;
+    [SerializeField] private string triggerID;
 
-    // Inside your DialogueTrigger.cs
     void Start() 
     {
         // If this trigger is meant to start as soon as the level loads
-        StartCoroutine(WaitToStart());
+        if (PlayerPrefs.GetInt(triggerID, 0) == 0) 
+        {
+            StartCoroutine(WaitToStart());
+        };
     }
 
     private IEnumerator WaitToStart() 
     {
+        PlayerPrefs.SetInt(triggerID, 1);
+        PlayerPrefs.Save();
         // Wait until the very end of the first frame
         yield return new WaitForEndOfFrame();
         DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
